7af9d980d333e416620966109460c30ab4cb2789
kino-ma <kino.ma.ms@gmail.com>
2020-06-29 22:16:48 +0900

    usersテーブルとkadaiテーブルの追加

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,10 +1,22 @@
 CREATE DATABASE IF NOT EXISTS db;
 USE db;
-CREATE TABLE IF NOT EXISTS info(
+
+
+CREATE TABLE IF NOT EXISTS users(
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    kg VARCHAR(10) NOT NULL,
-    login_name VARCHAR(15) NOT NULL
+    login_name VARCHAR(30) NOT NULL,
 );
 
-INSERT INTO info(name, kg, login_name) VALUES ("Seiki Makino", "ONE", "kino-ma");
+INSERT INTO user(login_name) VALUES ("kino-ma");
+
+
+CREATE TABLE IF NOT EXISTS kadai(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(11) NOT NULL,
+    title VARCHAR(40) NOT NULL,
+    content VARCHAR(200) NOT NULL,
+    draft VARCHAR(30),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+);
+
+INSERT INTO kadai(user_id, title, content) VALUES (1, "jn_lecture", "グループでWebサービスを作る");
