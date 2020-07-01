de93f21abdf57f013940886c3ed455a5e71ac38c
kino-ma <kino.ma.ms@gmail.com>
2020-07-01 20:18:09 +0900

    [kino-ma] add release pages

---
+++ app/build/precache-manifest.8a039254c1a39584030f4fc6ef9743b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd12205359dc6e1b427aa0272055581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "526e037cf1c49aac5d0b",
+    "url": "/static/css/main.e0ec40f1.chunk.css"
+  },
+  {
+    "revision": "6fd38c2eadbb35428082",
+    "url": "/static/js/2.00604269.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00604269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526e037cf1c49aac5d0b",
+    "url": "/static/js/main.bf6b39ad.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
